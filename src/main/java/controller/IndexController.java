5fca3297ed1d4ac5d17e9ff3ea560e6921c57092
mohdsyahrir <mohdsyahrir@192.168.0.163>
2014-06-10 15:53:41 +0800

    update baru

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
 
+	@RequestMapping("/index")
+	public String index(){
+		return "WEB-INF/jsp/index.jsp";
+	}
 }
